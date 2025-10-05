803d65e589bbfcf1d01106fefc9a76e87e88c09b
Manas <194718544+sdemanas@users.noreply.github.com>
2025-10-05 11:51:00 +0530

    refactor(angular): init Angular SPA

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,20 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Home } from './home/home';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
+  imports: [Home],
+  template: `
+    <main>
+      <header class="brand-name">
+        <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true" />
+      </header>
+      <section class="content">
+        <app-home></app-home>
+      </section>
+    </main> 
+  `,
   styleUrl: './app.css'
 })
 export class App {
